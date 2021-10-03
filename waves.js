de5121b02e263c4ab9f734481a21d3096babca03
momoladebrouill <momoladebrouill@gmail.com>
2021-10-03 17:45:05 +0200

    ajout de la citaiotn de flo et changement de l'animation background !

--- waves.js
+++ waves.js
@@ -16,47 +16,25 @@ function scroll(e) {
 	let val=Math.sign(e.deltaY)
 	/*for (var i = bulles.length - 1; i >= 0; i--) {
 		bulles[i].vec[1]-=val*10
-	}
-	indexBul++*/
-	if(val>0){
-		bulles.push(new Circle(indexBul))
-	}else{
-		bulles.shift()
-	}
+	}*/
+	bulles.push(new Circle())
 	
 	
 }
 class Circle{
-	constructor(ind){
-		this.pos=ind
-		this.x=Math.random()*W
-		this.y=Math.random()*H
-		this.vec=[(Math.random()-.5)*2*10,(Math.random()-.5)*2*10]
+	constructor(){
+		this.x=W/2
+		this.y=H/2
+		this.ang=Math.random()*2*Math.PI
 		this.ray=Math.random()*50
 		this.color="hsl("+Math.random()*360+",100%,50%)"
 	}
 	move(){
-		this.x+=this.vec[0]
-		if(this.x>W || this.x<0){
-			this.vec[0]*=-1
-			this.x+=this.vec[0]
-			if(this.x>W || this.x<0){
-				this.remove()
-			}else{
-				this.vec[0]*=0.9
-			}
+		this.x+=Math.cos(this.ang)
+		this.y+=Math.sin(this.ang)
+		if(this.x<0 || this.x>W || this.y<0 || this.y>H){
+			this.remove()
 		}
-		this.y+=this.vec[1]
-		if(this.y>H || this.y<0){
-			this.vec[1]*=-1
-			this.y+=this.vec[1]
-			if(this.y>H || this.y<0){
-				this.remove()
-			}else{
-				this.vec[1]*=0.9
-			}
-		}
-		this.vec[1]+=this.ray/25
 
 	}
 	draw(){
@@ -65,26 +43,29 @@ class Circle{
 		context.fillStyle=this.color
 		context.beginPath()
 		context.arc(this.x,this.y,this.ray,0,2*Math.PI)
-		//context.fillRect(t,this.ray,this.ray)
-		
 		context.closePath()
 		context.fill()
 	}
+
 	remove(){
-		bulles.splice(this.pos,1)
-		for (var i = bulles.length - 1; i > this.pos; i--) {
-			bulles[i].pos--
+		for (var i = bulles.length - 1; i >= 0; i--) {
+			if(bulles[i]==this){
+				bulles.splice(i,1);
+				break;
+			}
 		}
 	}
 }
 let bulles=[]
-let indexBul=0
+
 for (var i =0; i <10; i++) {
-	bulles.push(new Circle(i))
+	bulles.push(new Circle())
 }
 function Bloop() {
 	context.fillStyle="black"
 	context.fillRect(0,0,W,H)
+	context.fillStyle="red"
+	context.fillText(10,10,"r")
 	for (var i = bulles.length - 1; i >= 0; i--) {
 		bulles[i].draw()
 	}
